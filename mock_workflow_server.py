7fca66db3aa1696fc66352863fb8e9b10fb2f263
John Harrison <john@bloomonkey.co.uk>
2014-06-04 13:22:31 +0100

    read component host and/or port from env var
    The host and port at which component handler URLs are found can be
    specified in the environmental variables `PLANK_HOST` and
    `PLANK_PORT`. These are then substituted into the JSON before
    serving.

--- mock_workflow_server.py
+++ mock_workflow_server.py
@@ -5,6 +5,8 @@ Delivers dummy JSON for workflows that it reads from workflows.json
 
 from __future__ import with_statement
 
+import os
+
 try:
     import json
 except ImportError:
@@ -39,13 +41,18 @@ def get_workflow(identifier):
     """Get and return the requested workflow."""
     global workflows
     try:
+        wf = workflows[identifier]
+    except KeyError:
+        abort(404)
+    else:
+        # Replace URL with values from environment
+        for i, wf_item in enumerate(wf['wf']):
+            wf['wf'][i] = update_url(wf_item)
         return (
-            json.dumps(workflows[identifier]),
+            json.dumps(wf),
             200,
             {'Content-Type': "application/json"}
         )
-    except KeyError:
-        abort(404)
 
 
 @app.route('/workflows/<identifier>/<int:step>', methods=['GET'])
@@ -53,13 +60,39 @@ def get_workflow_step(identifier, step):
     """Get and return the requested workflow step."""
     global workflows
     try:
+        wf_item = workflows[identifier]['wf'][step]
+    except (KeyError, IndexError):
+        abort(404)
+    else:
+        # Replace URL with values from environment
+        wf_item = update_url(wf_item)
         return (
-            json.dumps(workflows[identifier]['wf'][step]),
+            json.dumps(wf_item),
             200,
             {'Content-Type': "application/json"}
         )
-    except (KeyError, IndexError):
-        abort(404)
+
+
+def update_url(wf_item):
+    """Update URL for a workflow step and return the step."""
+    global COMPONENT_HOST, COMPONENT_PORT
+    if COMPONENT_HOST is not None:
+        if COMPONENT_PORT is not None:
+            wf_item['url'] = wf_item['url'].replace(
+                '://127.0.0.1',
+                '://{0}:{1}'.format(COMPONENT_HOST, COMPONENT_PORT)
+            )
+        else:
+            wf_item['url'] = wf_item['url'].replace(
+                '://127.0.0.1',
+                '://{0}'.format(COMPONENT_HOST)
+            )
+    if COMPONENT_PORT is not None:
+        wf_item['url'] = wf_item['url'].replace(
+                '://127.0.0.1',
+                '://127.0.0.1:{0}'.format(COMPONENT_PORT)
+            )
+    return wf_item
 
 
 # Load dummy workflows from json
@@ -70,6 +103,9 @@ workflows = {wf['id']: wf for wf in workflow_list}
 
 
 app.debug = False
+
+COMPONENT_HOST = os.environ.get('PLANK_HOST')
+COMPONENT_PORT = os.environ.get('PLANK_PORT')
 
 
 if __name__ == '__main__':
